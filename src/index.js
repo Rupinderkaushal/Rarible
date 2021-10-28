96af4d2ce703505ab77a211a4b646dca9e0dfd91
Rupinder Kaushal <89014750+Rupinderkaushal@users.noreply.github.com>
2021-10-28 10:36:33 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
-
-import "../node_modules/mdb-ui-kit/css/mdb.min.css"
+import "../node_modules/mdb-ui-kit/css/mdb.min.css";
 import App from './App';
 // import reportWebVitals from './reportWe/bVitals';
 
